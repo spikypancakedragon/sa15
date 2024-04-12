329a0770b5e48fe1e2bb58c020eed6d7078d9303
Avery Walker <spikypancakedragon@gmail.com>
2024-04-12 10:31:27 -0500

    changed questions to multiple choice with radio buttons

--- script.js
+++ script.js
@@ -1,38 +1,20 @@
-const form = document.getElementById("form");
+const button = document.getElementById("button")
+
 const scoreDiv = document.getElementById("hidden");
 const scorep = document.getElementById("scorep");
 
-form.addEventListener("submit", function(event) {
+button.addEventListener("click", function(event) {
     event.preventDefault();
+    
+    numRight = 0
+    var rightAnswers = document.getElementsByClassName("right")
+    for (i = 0; i < rightAnswers.length; i++) {
+        if (rightAnswers[i].checked) {
+            numRight ++
+        }
+    }
+
     scoreDiv.style.display = "block";
-
-    const a1 = document.getElementById("q1").value;
-    const a2 = document.getElementById("q2").value;
-    const a3 = document.getElementById("q3").value;
-    const a4 = document.getElementById("q4").value;
-    const a5 = document.getElementById("q5").value;
-
-    var numRight = 0
-
-    if (a1 == "10") {
-        numRight ++
-    }
-    if (a2 == "120") {
-        numRight ++
-    }
-    if (a3 == "28") {
-        numRight ++
-    }
-    if (a4 == "5") {
-        numRight ++
-    }
-    if (a5 == "3") {
-        numRight ++
-    }
-
-
     scorep.textContent = `${numRight}/5 correct`
-
-
     
 });
